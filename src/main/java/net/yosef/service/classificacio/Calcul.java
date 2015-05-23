889989d453e7e2293c8bc298a17b756a7c6f2e2e
yoseef <yoseef@gmail.com>
2015-05-23 16:38:24 +0200

    calcul classificacio

---
+++ src/main/java/net/yosef/service/classificacio/Calcul.java
@@ -0,0 +1,16 @@
+package net.yosef.service.classificacio;
+
+import net.yosef.domain.Equip;
+import net.yosef.domain.Partit;
+
+/**
+ * Created by user on 23/05/15.
+ */
+public class Calcul {
+    void decidirGuanyador(Partit p){
+
+    }
+    void sumarPunts(Equip e){
+
+    }
+}
---
+++ src/main/java/net/yosef/service/lliga/GeneradorJornades.java
@@ -0,0 +1,7 @@
+package net.yosef.service.lliga;
+
+/**
+ * Created by user on 23/05/15.
+ */
+public class GeneradorJornades {
+}
